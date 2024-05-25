3d74138b5e4cf1f5d8f04aff9cabadfc0d9a50ec
alvexx45 <bernardoalves222@gmail.com>
2024-05-25 16:53:17 -0300

    tela de ediçao de dados - bernardo alves

--- codigo/assets/js/painel.js
+++ codigo/assets/js/painel.js
@@ -1,33 +1,17 @@
-// Dados a serem armazenados no Local Storage
-const painelData = {
-    nome: "SmartFit - Unidade XYZ",
-    endereco: "Rua da Água, 29",
-    descricao: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut nulla neque repellat eos obcaecati doloribus vitae laboriosam, ullam ea perspiciatis error sed magnam accusamus est ad, rem facilis voluptates rerum. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vel atque iusto, tempora autem quos quas inventore asperiores quidem officia repellat perspiciatis ex. Cumque sint nesciunt ipsum est molestias nobis aliquam.",
-    mainPhoto: "../assets/img/logo.png",
-    photos: [
-        "../assets/img/esteira.png",
-        "../assets/img/acad.png",
-        "../assets/img/smart-blue.png",
-        "../assets/img/smart-esteira.png",
-        "../assets/img/acad.png",
-        "../assets/img/smart-blue.png",
-        "../assets/img/smart-esteira.png",
-        "../assets/img/smart-outside.jpg"
-    ]
-};
+document.addEventListener('DOMContentLoaded', function() {
+    const storedData = JSON.parse(localStorage.getItem('painelData'));
 
-// Armazenar os dados no Local Storage
-localStorage.setItem('painelData', JSON.stringify(painelData));
+    if (storedData) {
+        document.getElementById('nome').textContent = `Nome: ${storedData.nome}`;
+        document.getElementById('endereco').textContent = `Endereço: ${storedData.endereco}`;
+        document.getElementById('descricao').textContent = `Descrição: ${storedData.descricao}`;
+        document.getElementById('mainPhoto').src = storedData.mainPhoto;
 
-// Recuperar os dados do Local Storage
-const storedData = JSON.parse(localStorage.getItem('painelData'));
-
-// Exibir os dados na página HTML
-document.getElementById('nome').textContent = `Nome: ${storedData.nome}`;
-document.getElementById('endereco').textContent = `Endereço: ${storedData.endereco}`;
-document.getElementById('descricao').textContent = `Descrição: ${storedData.descricao}`;
-document.getElementById('mainPhoto').src = storedData.mainPhoto;
-
-storedData.photos.forEach((photo, index) => {
-    document.getElementById(`foto${index + 1}`).querySelector('img').src = photo;
+        storedData.photos.forEach((photo, index) => {
+            const photoElement = document.getElementById(`foto${index + 1}`);
+            if (photoElement) {
+                photoElement.querySelector('img').src = photo;
+            }
+        });
+    }
 });
